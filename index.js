200f4dd21d610ec42d77d9da696d118cf02f2ec8
Jm <jonathan.p.middleton@gmail.com>
2019-03-18 21:03:17 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 /* code goes here */
+document.addEventListener("DOMContentLoaded", e => {
+  document.querySelector("body")
+   .addEventListener("click", e => console.log("Reggae, Reggae!"));
+});
+
+document.addEventListener("DOMContentLoaded", e => {
+  document.querySelector("div#hidden-div").innerText="301";
+});
