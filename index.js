449d0a8270d90a22d0ca05254146f7fe08304b4b
Jm <jonathan.p.middleton@gmail.com>
2019-03-18 21:01:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 /* code goes here */
+document.addEventListener("DOMContentLoaded", e => {
+  document.querySelector("body")
+   .addEventListener("click", e => console.log("Reggae, Reggae!"));
+});
+
+document.addEventListener("DOMContentLoaded", e => {
+  document.querySelector("div#hidden-div");
+   addEventListener("innerText", e => "301");
+});
